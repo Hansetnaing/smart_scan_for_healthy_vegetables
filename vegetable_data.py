73703fbb1c7e91f96c3e59cf7a3d27f2611e485b
Hansetnaing <minhansetnaing11@gmail.com>
2026-02-15 10:01:30 +0630

    Adding Vegetable Data File

---
+++ vegetable_data.py
@@ -0,0 +1,66 @@
+# vegetable_data.py
+
+vegetable_info = {
+    "Potato": {
+        "calories": "77 kcal",
+        "nutrient": "Vitamin C, B6, Potassium",
+        "benefit1": "Energy booster",
+        "benefit2": "Good for digestion"
+    },
+
+    "Tomato": {
+        "calories": "18 kcal",
+        "nutrient": "Vitamin A, C, Lycopene",
+        "benefit1": "Good for heart",
+        "benefit2": "Rich in antioxidants"
+    },
+
+    "Cucumber": {
+        "calories": "16 kcal",
+        "nutrient": "Vitamin K",
+        "benefit1": "Hydrates body",
+        "benefit2": "Good for skin"
+    },
+
+    "Ladyfinger": {
+        "calories": "33 kcal",
+        "nutrient": "Fiber",
+        "benefit1": "Improves digestion",
+        "benefit2": "Controls blood sugar"
+    },
+
+    "Lettuce": {
+        "calories": "15 kcal",
+        "nutrient": "Vitamin A, K",
+        "benefit1": "Good for skin",
+        "benefit2": "Supports hydration"
+    },
+
+    "Carrot": {
+        "calories": "41 kcal",
+        "nutrient": "Vitamin A (Beta-carotene)",
+        "benefit1": "Improves eyesight",
+        "benefit2": "Boosts immunity"
+    },
+
+    "Chili Red": {
+        "calories": "40 kcal",
+        "nutrient": "Vitamin C",
+        "benefit1": "Boosts metabolism",
+        "benefit2": "Improves circulation"
+    },
+
+    "Chili Green": {
+        "calories": "30 kcal",
+        "nutrient": "Vitamin C",
+        "benefit1": "Improves digestion",
+        "benefit2": "Rich in antioxidants"
+    },
+
+    "Lime": {
+        "calories": "30 kcal",
+        "nutrient": "Vitamin C",
+        "benefit1": "Boosts immunity",
+        "benefit2": "Improves digestion"
+    }
+}
